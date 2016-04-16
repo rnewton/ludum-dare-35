bf4fe7b9b6b03174b70eaab937dcbba940a27980
stu <shoeberto@gmail.com>
2016-04-16 16:54:25 -0700

    Dodecagram logic, plus more enemy AI.

---
+++ Assets/Scripts/Dodecagram/DodecagramPiece.cs
@@ -0,0 +1,5 @@
+﻿using UnityEngine;using System.Collections;public class DodecagramPiece : MonoBehaviour {	// Use this for initialization	void Start () {		}		// Update is called once per frame	void Update () {		}    void OnTriggerEnter2D(Collider2D collider)    {        GameObject obj = collider.gameObject;        if (obj.CompareTag("Enemy"))        {            if (obj.transform.GetComponentInChildren<DodecagramPiece>() == null)
+            {
+                this.transform.SetParent(obj.transform);
+                obj.GetComponent<BlobState>().behaviors.Switch(BlobState.states.Fleeing);
+            }        }    }}
--- Assets/Scripts/Enemy/BlobBehavior.cs
+++ Assets/Scripts/Enemy/BlobBehavior.cs
@@ -1,48 +1,5 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class BlobBehavior : MonoBehaviour {
-	public float speed = 600f;
-
-	private Vector3 moveDirection = Vector3.zero;
-	private Rigidbody2D rigidBody;
-
-	private float minX;
-	private float maxX;
-	private float minY;
-	private float maxY;
-
-	void Start()
-	{
-		// Get a reference to the rigidbody attached to the player
-		rigidBody = GetComponent<Rigidbody2D>();
-
-		// Set values for constraining movement within the camera view
-		float xHalfDistance = Camera.main.orthographicSize * Camera.main.aspect;
-		float yHalfDistance = Camera.main.orthographicSize;
-
-		minX = Camera.main.transform.position.x - xHalfDistance + 0.5f;
-		maxX = Camera.main.transform.position.x + xHalfDistance - 0.5f;
-
-		minY = Camera.main.transform.position.y - yHalfDistance + 0.4f;
-		maxY = Camera.main.transform.position.y + yHalfDistance - 0.5f;
-	}
-
-
-	// Update is called once per frame
-	void Update () {
-
-	}
-
-    private void BaseUpdate()
+﻿using UnityEngine;using System.Collections;using System;public class BlobBehavior : MonoBehaviour {	public float speed = 600f;    public float playerDistance = 5f;    public float playerFrightVariation = 25f;    public float maxVelocity = 25f;	private Vector3 moveDirection = Vector3.zero;	private Rigidbody2D rigidBody;    private GameObject player;	private float minX;	private float maxX;	private float minY;	private float maxY;	void Start()	{		// Get a reference to the rigidbody attached to the player		rigidBody = GetComponent<Rigidbody2D>();        player = GameObject.Find("Player");		// Set values for constraining movement within the camera view		float xHalfDistance = Camera.main.orthographicSize * Camera.main.aspect;		float yHalfDistance = Camera.main.orthographicSize;		minX = Camera.main.transform.position.x - xHalfDistance + 0.5f;		maxX = Camera.main.transform.position.x + xHalfDistance - 0.5f;		minY = Camera.main.transform.position.y - yHalfDistance + 0.4f;		maxY = Camera.main.transform.position.y + yHalfDistance - 0.5f;	}	// Update is called once per frame	void Update () {	}    private void constrainMovement()
     {
-        // Use input up and down for direction, multiplied by speed
-        moveDirection = getMoveDir();
-		moveDirection *= speed;
-
-		// Move Rigidbody
-		rigidBody.AddRelativeForce(moveDirection * Time.deltaTime);
-
 		// Constrain to camera viewport
 		float distance = Vector2.Distance (transform.position, Vector2.zero);
 
@@ -57,15 +14,13 @@ public class BlobBehavior : MonoBehaviour {
 		} else if (rigidBody.position.y > maxY) {
 			rigidBody.AddForce(Vector2.down * speed * distance * Time.deltaTime);
 		}
-    }
-
-    private Vector2 getMoveDir()
-    {
-        return -gameObject.transform.position.normalized;
-    }
-
-    public void Attack()
-    {
-        BaseUpdate();
-    }
-}
+    }    public void Attack()    {        if (rigidBody.velocity.magnitude > maxVelocity)
+        {
+            moveDirection = -0.5f * rigidBody.velocity;
+        } else
+        {
+            moveDirection = -gameObject.transform.position.normalized;        }        Vector2 playerVector = rigidBody.position - player.GetComponent<Rigidbody2D>().position;        if (Math.Abs(playerVector.magnitude) <= playerDistance)        {            moveDirection += new Vector3(UnityEngine.Random.Range(0f, playerFrightVariation), UnityEngine.Random.Range(0f, playerFrightVariation), 0);        }		moveDirection *= speed;		// Move Rigidbody		rigidBody.AddRelativeForce(moveDirection * Time.deltaTime);        constrainMovement();    }    public void Flee()    {        if (rigidBody.velocity.magnitude > maxVelocity)        {            moveDirection = -0.5f * rigidBody.velocity;        }
+        else        {            float closestXEdge = Math.Abs(rigidBody.position.x - minX) < Math.Abs(rigidBody.position.x - maxX) ? minX : maxX;            float closestYEdge = Math.Abs(rigidBody.position.y - minY) < Math.Abs(rigidBody.position.y - maxY) ? minY : maxY;            moveDirection = rigidBody.position - new Vector2(closestXEdge, closestYEdge);            moveDirection.Normalize();
+        }        Vector2 playerVector = rigidBody.position - player.GetComponent<Rigidbody2D>().position;        if (Math.Abs(playerVector.magnitude) <= playerDistance)
+        {
+            moveDirection += new Vector3(UnityEngine.Random.Range(0f, playerFrightVariation), UnityEngine.Random.Range(0f, playerFrightVariation), 0);        }        moveDirection *= speed;		rigidBody.AddRelativeForce(moveDirection * Time.deltaTime);        constrainMovement();    }}
