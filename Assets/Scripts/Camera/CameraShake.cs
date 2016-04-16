413cb26852fe04d6692f27cbe46020ad6c734a3b
Robert Newton <robert.newton11@gmail.com>
2016-04-16 18:32:27 -0400

    The game aint a game without camera shake

---
+++ Assets/Scripts/Camera/CameraShake.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraShake : MonoBehaviour
+{
+	// How long the object should shake for.
+	private float shakeDuration = 0f;
+
+	// Amplitude of the shake. A larger value shakes the camera harder.
+	private float shakeAmount = 0.7f;
+	private float decreaseFactor = 1.0f;
+
+	private Transform camTransform;
+	private Vector3 originalPos;
+
+	void Awake()
+	{
+		if (camTransform == null){
+			camTransform = Camera.main.transform;
+		}
+	}
+
+	void OnEnable()
+	{
+		originalPos = camTransform.localPosition;
+	}
+
+	void Update()
+	{
+		if (shakeDuration > 0) {
+			camTransform.localPosition = originalPos + Random.insideUnitSphere * shakeAmount;
+			shakeDuration -= Time.deltaTime * decreaseFactor;
+		} else {
+			shakeDuration = 0f;
+			camTransform.localPosition = originalPos;
+		}
+	}
+
+	public void Shake(float duration, float amount, float decFactor)
+	{
+		this.shakeDuration = duration;
+		this.shakeAmount = amount;
+		this.decreaseFactor = decFactor;
+	}
+}
--- Assets/Scripts/Player/PlayerAttacks.cs
+++ Assets/Scripts/Player/PlayerAttacks.cs
@@ -4,15 +4,18 @@ using System.Collections;
 public class PlayerAttacks : MonoBehaviour
 {
 	private MovePlayer movement;
+	private CameraShake cameraShake;
 
 	void Start ()
 	{
 		movement = GetComponent<MovePlayer> ();
+		cameraShake = GetComponent<CameraShake> ();
 	}
 
 	void OnCollisionEnter2D(Collision2D c)
 	{
 		if (c.gameObject.CompareTag ("Enemy") && movement.Attacking) {
+			cameraShake.Shake (0.5f, 0.1f, 1f);
 			Destroy (c.gameObject);
 		}
 	}
