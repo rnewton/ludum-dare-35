01eec6c23a816254d35587cd75c353dde4afdfa7
Robert Newton <robert.newton11@gmail.com>
2016-04-16 12:33:44 -0400

    Added coin pickup

---
+++ Assets/Scripts/Player/PlayerCoins.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PlayerCoins : MonoBehaviour
+{
+	public int Coins { get; private set; }
+
+	public void AddCoin()
+	{
+		Coins++;
+	}
+
+	void OnTriggerEnter2D(Collider2D c)
+	{
+		if (c.gameObject.CompareTag ("Coin")) {
+			AddCoin ();
+			Destroy (c.gameObject);
+		}
+	}
+}
