56c0075ec608590f82768b062374b09bd93e8476
Robert Newton <robert.newton11@gmail.com>
2016-04-16 21:49:04 -0400

    Added splat sound effect for CRUSHING JUICINESS

---
+++ Assets/Scripts/SoundEffectManager/SoundEffectManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(AudioSource))]
+public class SoundEffectManager : MonoBehaviour
+{
+	public AudioClip[] clips;
+
+	private Dictionary<string, AudioClip> namedClips;
+	private AudioSource source;
+
+	void Start()
+	{
+		namedClips = new Dictionary<string, AudioClip> ();
+		foreach (AudioClip clip in clips) {
+			namedClips.Add (clip.name, clip);
+		}
+
+		source = GetComponent<AudioSource> ();
+	}
+
+	public void PlayClip(string name)
+	{
+		Debug.Log (name);
+		if (namedClips.ContainsKey (name)) {
+			source.PlayOneShot (namedClips [name]);
+		} else {
+			Debug.LogError ("Tried to play clip '" + name + "', but I don't have anything named that");
+		}
+	}
+}
