3b53a78bce8477a577183dd23fbe3596e6936829
Robert Newton <robert.newton11@gmail.com>
2016-04-16 20:59:02 -0400

    Prices increase as you buy shapes

--- Assets/Scripts/Store/StoreManager.cs
+++ Assets/Scripts/Store/StoreManager.cs
@@ -10,6 +10,22 @@ public class StoreManager : MonoBehaviour, IPointerEnterHandler, IPointerExitHan
 	public Text UICoinCount;
 	public Animator StoreAnimator;
 
+	public Text TriangleBuyButtonLabel;
+	public Text SquareBuyButtonLabel;
+	public Text HexagonBuyButtonLabel;
+
+	public int baseTrianglePrice = 5;
+	public int baseSquarePrice = 10;
+	public int baseHexagonPrice = 15;
+
+	public float trianglePriceMultiplier = 1.5f;
+	public float squarePriceMultiplier = 1.5f;
+	public float hexagonPriceMultiplier = 1.5f;
+
+	private int currentTrianglePrice;
+	private int currentSquarePrice;
+	private int currentHexagonPrice;
+
 	public void AddCoin()
 	{
 		Coins++;
@@ -20,8 +36,21 @@ public class StoreManager : MonoBehaviour, IPointerEnterHandler, IPointerExitHan
 		Coins += 1000;
 	}
 
-	public void SpendCoins(int howMany)
+	public void SpendCoins(string which)
 	{
+		int howMany = int.MaxValue; // Bajillion
+		switch (which) {
+		case "triangle":
+			howMany = currentTrianglePrice;
+			break;
+		case "square":
+			howMany = currentSquarePrice;
+			break;
+		case "hexagon":
+			howMany = currentHexagonPrice;
+			break;
+		}
+
 		if (howMany > Coins) {
 			Purchased = false;
 			return;
@@ -29,6 +58,11 @@ public class StoreManager : MonoBehaviour, IPointerEnterHandler, IPointerExitHan
 
 		Coins -= howMany;
 		Purchased = true;
+
+		// Apply multipliers!
+		currentTrianglePrice = Mathf.RoundToInt (currentTrianglePrice * trianglePriceMultiplier);
+		currentSquarePrice = Mathf.RoundToInt (currentSquarePrice * squarePriceMultiplier);
+		currentHexagonPrice = Mathf.RoundToInt (currentHexagonPrice * hexagonPriceMultiplier);
 	}
 
 	public void OnPointerEnter(PointerEventData eventData)
@@ -41,8 +75,18 @@ public class StoreManager : MonoBehaviour, IPointerEnterHandler, IPointerExitHan
 		StoreAnimator.SetBool ("Hover", false);
 	}
 
+	void Start()
+	{
+		currentTrianglePrice = baseTrianglePrice;
+		currentSquarePrice = baseSquarePrice;
+		currentHexagonPrice = baseHexagonPrice;
+	}
+
 	void Update()
 	{
 		UICoinCount.text = Coins.ToString ();
+		TriangleBuyButtonLabel.text = currentTrianglePrice.ToString ();
+		SquareBuyButtonLabel.text = currentSquarePrice.ToString ();
+		HexagonBuyButtonLabel.text = currentHexagonPrice.ToString ();
 	}
 }
