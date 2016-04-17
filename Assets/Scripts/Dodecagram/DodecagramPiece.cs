84d5162cdb4eeb359ac971d62bc24f5662c9a91c
stu <shoeberto@gmail.com>
2016-04-16 19:41:52 -0700

    Random coin drops; multiple state behavior for dodecagram pieces; more toying with enemy AI.

--- Assets/Scripts/Dodecagram/DodecagramPiece.cs
+++ Assets/Scripts/Dodecagram/DodecagramPiece.cs
@@ -1,5 +1,35 @@
-﻿using UnityEngine;using System.Collections;public class DodecagramPiece : MonoBehaviour {	// Use this for initialization	void Start () {		}		// Update is called once per frame	void Update () {		}    void OnTriggerEnter2D(Collider2D collider)    {        GameObject obj = collider.gameObject;        if (obj.CompareTag("Enemy"))        {            if (obj.transform.GetComponentInChildren<DodecagramPiece>() == null)
+﻿using UnityEngine;using CoinFlipGames.FSM;using System.Collections;public class DodecagramPiece : MonoBehaviour {    public states state;    public enum states    {        Home,        Stolen,        Dropped    }    private Vector3 homePosition;    private Quaternion homeRotation;	// Use this for initialization	void Start () {        homePosition = gameObject.transform.position;        homeRotation = gameObject.transform.rotation;	}		// Update is called once per frame	void Update () {		}    void OnTriggerEnter2D(Collider2D collider)    {        // allow for 'emergent' behavior; blobs can steal from blobs
+        if (state == states.Home || state == states.Stolen)
+        {
+            collideWithEnemy(collider);
+        } else if (state == states.Dropped)
+        {
+            collideDropped(collider);
+        }
+    }    void collideWithEnemy(Collider2D collider)
+    {
+        GameObject obj = collider.gameObject;
+
+        if (obj.CompareTag("Enemy"))
+        {
+            if (obj.transform.GetComponentInChildren<DodecagramPiece>() == null)
             {
                 this.transform.SetParent(obj.transform);
                 obj.GetComponent<BlobState>().behaviors.Switch(BlobState.states.Fleeing);
-            }        }    }}
+                state = states.Stolen;
+            }
+        }
+    }    void collideDropped(Collider2D collider)
+    {
+        MovePlayer player = collider.gameObject.GetComponentInParent<MovePlayer>();
+
+        if (player != null)
+        {
+            gameObject.transform.position = homePosition;
+            gameObject.transform.rotation = homeRotation;
+            state = states.Home;
+        } else
+        {
+            collideWithEnemy(collider);
+        }
+    }}
--- Assets/Scripts/Enemy/BlobBehavior.cs
+++ Assets/Scripts/Enemy/BlobBehavior.cs
@@ -4,32 +4,48 @@
 		float distance = Vector2.Distance (transform.position, Vector2.zero);
 
 		if (rigidBody.position.x < minX) {
-			rigidBody.AddForce(Vector2.right * speed * distance * Time.deltaTime);
+			rigidBody.AddForce(Vector2.right * speed * distance * Time.deltaTime * rigidBody.mass);
 		} else if (rigidBody.position.x > maxX) {
-			rigidBody.AddForce(Vector2.left * speed * distance * Time.deltaTime);
+			rigidBody.AddForce(Vector2.left * speed * distance * Time.deltaTime * rigidBody.mass);
 		}
 
 		if (rigidBody.position.y < minY) {
-			rigidBody.AddForce(Vector2.up * speed * distance * Time.deltaTime);
+			rigidBody.AddForce(Vector2.up * speed * distance * Time.deltaTime * rigidBody.mass);
 		} else if (rigidBody.position.y > maxY) {
-			rigidBody.AddForce(Vector2.down * speed * distance * Time.deltaTime);
+			rigidBody.AddForce(Vector2.down * speed * distance * Time.deltaTime * rigidBody.mass);
 		}
     }    private void dampen()
     {
         float velocityMag = Math.Abs(rigidBody.velocity.magnitude);
-        if (velocityMag > maxVelocity)
+
+        // stop entirely if we're going caraaaaaazy
+        if (velocityMag > 2f * maxVelocity)
+        {
+            rigidBody.velocity = Vector3.zero;
+            rigidBody.angularVelocity = 0f;
+        } else if (velocityMag > maxVelocity)
         {
             rigidBody.AddRelativeForce(-1.0f * (1f - (velocityMag / maxVelocity)) * rigidBody.velocity * rigidBody.mass);
         }
-    }    public void Attack()    {        dampen();
-        moveDirection = -gameObject.transform.position.normalized;        Vector2 playerVector = rigidBody.position - player.GetComponent<Rigidbody2D>().position;        if (Math.Abs(playerVector.magnitude) <= playerDistance)        {            moveDirection += new Vector3(UnityEngine.Random.Range(0f, playerFrightVariation), UnityEngine.Random.Range(0f, playerFrightVariation), 0);        }		moveDirection *= speed;		// Move Rigidbody		rigidBody.AddRelativeForce(moveDirection * Time.deltaTime);        constrainMovement();    }    public void Flee()    {        dampen();        float closestXEdge = Math.Abs(rigidBody.position.x - minX) < Math.Abs(rigidBody.position.x - maxX) ? minX : maxX;        float closestYEdge = Math.Abs(rigidBody.position.y - minY) < Math.Abs(rigidBody.position.y - maxY) ? minY : maxY;        moveDirection = rigidBody.position - new Vector2(closestXEdge, closestYEdge);        moveDirection.Normalize();
-        Vector2 playerVector = rigidBody.position - player.GetComponent<Rigidbody2D>().position;        if (Math.Abs(playerVector.magnitude) <= playerDistance)
+    }    public void Attack()    {        dampen();        moveDirection = -gameObject.transform.position.normalized;        Vector2 playerVector = rigidBody.position - player.GetComponent<Rigidbody2D>().position;        if (Math.Abs(playerVector.magnitude) <= playerDistance)        {            moveDirection += new Vector3(UnityEngine.Random.Range(0f, playerFrightVariation), UnityEngine.Random.Range(0f, playerFrightVariation), 0);        }		moveDirection *= speed;		// Move Rigidbody		rigidBody.AddRelativeForce(moveDirection * Time.deltaTime);        constrainMovement();    }    public void Flee()    {        dampen();        float closestXEdge = Math.Abs(rigidBody.position.x - minX) < Math.Abs(rigidBody.position.x - maxX) ? minX : maxX;        float closestYEdge = Math.Abs(rigidBody.position.y - minY) < Math.Abs(rigidBody.position.y - maxY) ? minY : maxY;        moveDirection = rigidBody.position - new Vector2(closestXEdge, closestYEdge);        moveDirection.Normalize();
+        Vector2 playerVector = rigidBody.position - player.GetComponent<Rigidbody2D>().position;        float playerVecMagnitude = Math.Abs(playerVector.magnitude);        if (playerVecMagnitude <= playerDistance)
         {
-            moveDirection += new Vector3(UnityEngine.Random.Range(0f, playerFrightVariation), UnityEngine.Random.Range(0f, playerFrightVariation), 0);        }        moveDirection *= speed;		rigidBody.AddRelativeForce(moveDirection * Time.deltaTime);        constrainMovement();    }    public void die()
+            moveDirection += new Vector3(                (-playerVector.x / playerVecMagnitude) * UnityEngine.Random.Range(-playerFrightVariation, playerFrightVariation),                 (-playerVector.y / playerVecMagnitude) * UnityEngine.Random.Range(-playerFrightVariation, playerFrightVariation),                 0           );        }        moveDirection *= speed;		rigidBody.AddRelativeForce(moveDirection * Time.deltaTime);        constrainMovement();    }    public void die()
     {
-        GameObject coin = (GameObject)Instantiate(coinPrefab, new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, -5), Quaternion.identity);
+        for (int i = 0; i < UnityEngine.Random.Range(1, 3); i++)
+        {
+            GameObject coin = (GameObject)Instantiate(coinPrefab, 
+                new Vector3(
+                    gameObject.transform.position.x + UnityEngine.Random.Range(-1f, 1f), 
+                    gameObject.transform.position.y + UnityEngine.Random.Range(-1f, 1f),
+                    -5
+                ), 
+                Quaternion.identity
+            );
+        }
 
         DodecagramPiece piece = gameObject.GetComponentInChildren<DodecagramPiece>();        if (piece != null)
         {
             piece.gameObject.transform.SetParent(dodecagram.transform);
+            piece.state = DodecagramPiece.states.Dropped;
         }    }}
