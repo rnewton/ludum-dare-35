be3448fa08f091e071bec5c7501b3b6fd6ac04ba
Robert Newton <robert.newton11@gmail.com>
2016-04-16 22:23:16 -0400

    Added coin pickup sound

--- Assets/Scripts/Player/PlayerCoins.cs
+++ Assets/Scripts/Player/PlayerCoins.cs
@@ -3,16 +3,19 @@
 public class PlayerCoins : MonoBehaviour
 {
 	private StoreManager store;
+	private SoundEffectManager soundEffectManager;
 
 	void Start ()
 	{
 		store = GameObject.Find ("GameUI").GetComponent<StoreManager> ();
+		soundEffectManager = GameObject.Find ("SoundEffectManager").GetComponent<SoundEffectManager> ();
 	}
 
 	void OnTriggerEnter2D(Collider2D c)
 	{
 		if (c.gameObject.CompareTag ("Coin")) {
 			store.AddCoin ();
+			soundEffectManager.PlayClip ("coin");
 			Destroy (c.gameObject);
 		}
 	}
